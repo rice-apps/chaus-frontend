01e76c394941969cdfbf12b526cb999649fb9afe
Jamie <jlt10@rice.edu>
2017-11-19 14:31:42 -0600

    no message

--- src/reducers/mCalReducer.js
+++ src/reducers/mCalReducer.js
@@ -5,10 +5,19 @@ import { combineReducers } from 'redux'
 
 
 
+const activeShiftReducer = (state={day:"",hour:0, closed:true, available:[], schedule:[]}, action) => {
+    switch(action.type) {
+        case "SHIFT_SELECTED":
+            return {...state, avaliable:action.avaliable, schedule:action.schedule}
+        default:
+            return state
+    }
 
+}
 
 
 const mCalReducer = combineReducers({
+    activeShiftReducer
 })
 
 
