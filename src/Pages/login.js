b0eda8fdec63c4e90f2fa195ab3db741f5c0c22d
danya3781 <da30@rice.edu>
2018-04-08 14:58:47 -0500

    added login page

---
+++ src/Pages/login.js
@@ -0,0 +1,34 @@
+/**
+ * Created by dandreini16 on 4/8/18.
+ */
+import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+const MyLink = props => <Link to="http://www.google.com" {...props} />
+
+const LOGINURL = 'https://idp.rice.edu/idp/profile/cas/login'
+const SERVICEURL = 'https://speak.riceapps.org/auth'
+
+const LoginPage = () => {
+    return (
+        <MuiThemeProvider>
+            <div style={{display: 'flex', paddingTop: 60, paddingLeft: 100}}>
+                <div style={{flexGrow: 0.3}} >
+                    <h1>
+                        Login
+                    </h1>
+                    <RaisedButton primary={true} linkButton={true}
+                                  href={LOGINURL + "?service=" + SERVICEURL} >
+                        Login
+                    </RaisedButton>
+                </div>
+
+
+            </div>
+        </MuiThemeProvider>
+    )
+}
+
+export default LoginPage
